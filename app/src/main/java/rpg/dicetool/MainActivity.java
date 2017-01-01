4d8f067ce0f45523bdf39ee3273321e09e6a234f
Corbi <corbinianbutz@web.de>
2017-01-01 17:10:55 +0100

    fixed errors

--- app/src/main/java/rpg/dicetool/MainActivity.java
+++ app/src/main/java/rpg/dicetool/MainActivity.java
@@ -13,10 +13,6 @@ import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,16 +21,10 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private BarChart chart;
-    private HashMap<String, List<BarEntry>> chartData;
+    private HashMap<String, BarDataSet> chartData;
 
     private int iterations = 1000000;
 
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +44,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        chartData = new HashMap<String, List<BarEntry>>();
+        chartData = new HashMap<String, BarDataSet>();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void executeSimulation(String expr)
@@ -84,68 +71,36 @@ public class MainActivity extends AppCompatActivity {
             }
 
             //store data in bar entry
-            chartData.put("expr", entries);
+            BarDataSet dataSet = new BarDataSet(entries, expr);
+            dataSet.setColor(Color.BLUE);
+            dataSet.setValueTextColor(Color.BLUE); // styling, ...
+
+            chartData.put(expr, dataSet);
         }
     }
 
     private void updateChart()
     {
+        BarData bardata = new BarData();
 
-        BarDataSet dataSet = new BarDataSet(entries, expr); // add entries to dataset
-        dataSet.setColor(Color.BLUE);
-        //dataSet.setValueTextColor(Color.BLUE); // styling, ...
-        BarDataSet dataSet2 = new BarDataSet(entries2, expr2); // add entries to dataset
-        dataSet.setColor(Color.GREEN);
+        for( String key : chartData.keySet())
+        {
+            BarDataSet set =  chartData.get(key);
 
-        BarData barData = new BarData(dataSet, dataSet2);
+            bardata.addDataSet(set);
+        }
 
         float groupSpace = 0.06f;
         float barSpace = 0.02f; // x2 dataset
         float barWidth = 0.45f; // x2 dataset
 
-        barData.setBarWidth(barWidth); // set the width of each bar
-        chart.setData(barData);
+        bardata.setBarWidth(barWidth); // set the width of each bar
+        chart.setData(bardata);
         chart.groupBars(0f, groupSpace, barSpace); // perform the "explicit" grouping
 
         XAxis xAxis = chart.getXAxis();
         xAxis.setCenterAxisLabels(true);
 
         chart.invalidate(); // refresh
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
